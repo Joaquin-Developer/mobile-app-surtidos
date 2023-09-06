8a36f034185368887b35ebc2a46669fdd27e6ffc
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2023-09-06 03:05:53 -0300

    pre-compra funcional

--- scripts/compra.js
+++ scripts/compra.js
@@ -1,4 +1,125 @@
-const btnSaveProduct = document.getElementById("saveProduct")
+const STORAGE_KEY_SURTIDOS = "all_surtido_products"
 
 
+class SurtidoProduct {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    saveProduct(product) {
+        let allProducts = this.getAllProducts()
+
+        if (!allProducts)
+            allProducts = new Array()
+
+        allProducts.push(product)
+        localStorage.setItem(STORAGE_KEY_SURTIDOS, JSON.stringify(allProducts))
+    }
+
+    insertProduct(product) {
+        let liProduct = document.createElement("li")
+        let inputCheck = document.createElement("input");
+        inputCheck.classList.add("form-check-input")
+        inputCheck.type = "checkbox"
+        inputCheck.id = "checkbox_" + product.name;
+        inputCheck.addEventListener("click", new SurtidoProduct().#event_updateProductStatus);
+
+        let label = document.createElement("label");
+        label.classList.add("form-check-label");
+        label.for = inputCheck.id;
+        label.appendChild(document.createTextNode(product.name));
+        label.addEventListener("dblclick", new SurtidoProduct().#event_removeProduct);
+
+        if (product.finished) {
+            label.classList.add("line-through");
+            inputCheck.checked = true;
+        }
+        liProduct.appendChild(inputCheck);
+        liProduct.appendChild(label);
+        document.getElementById("all_pre_products").appendChild(liProduct);
+    }
+
+    #event_removeProduct(evt) {
+        if (confirm("¿Seguro que deseas eliminar el producto de la lista?")) {
+            const label = evt.srcElement
+            const nameProduct = label.textContent
+            // remove product from Storage:
+            Product.removeProduct(nameProduct)
+            // remove product from view:
+            label.parentElement.parentElement.removeChild(label.parentElement)
+        }
+    }
+
+    #event_updateProductStatus(evt) {
+        const element = evt.srcElement;
+        let productName = element.id.split('_')[1];
+        // remove line from label:
+        const label = element.parentElement.childNodes[1]
+        // debugger;
+        if (element.checked) {
+            label.classList.add("line-through");
+        } else {
+            label.classList.remove("line-through");
+        }
+        // updated preProduct in local storage:
+        Product.updateProductStatus(productName);
+    }
+
+    removeProduct(type) {
+        const allProducts = getAllProducts(type)
+
+        for (let index in allProducts) {
+            if (allProducts[index].name == this.name) {
+                allProducts.splice(index, 1)
+                break
+            }
+        }
+        localStorage.setItem(STORAGE_KEY_SURTIDOS, JSON.stringify(allProducts))
+    }
+
+    static updateProductStatus(productName) {
+        const allProducts = this.getAllProducts()
+        for (let product of allProducts) {
+            if (product.name === productName) {
+                product.finished = (!product.finished)
+                break
+            }
+        }
+
+        localStorage.setItem(STORAGE_KEY_SURTIDOS, JSON.stringify(allProducts))
+    }
+
+}
+
+
+const btnInsertProduct = document.getElementById("saveProduct")
+const productsList = document.getElementById("products_list")
+const productNameImput = document.getElementById("productNameImput")
+const productPriceImput = document.getElementById("productPriceImput")
+
+
+btnInsertProduct.addEventListener("click", (evt) => {
+    evt.preventDefault()
+    const name = productNameImput.value
+    const price = productPriceImput.value
+
+    if (name && price) {
+        const product = new SurtidoProduct(name, price, false)
+        product.insertProduct(product)
+        product.saveProduct(product)
+
+        productNameImput.value = ""
+        productPriceImput.value = ""
+    }
+})
+
+
+addEventListener("DOMContentLoaded", () => {
+    const allProducts = SurtidoProduct.getAllProducts()
+
+    if (allProducts !== null && allProducts.length > 0) {
+        allProducts.forEach(product => SurtidoProduct.insertProduct(product))
+    }
+})
 
--- scripts/precompra.js
+++ scripts/precompra.js
@@ -1,38 +1,125 @@
+const STORAGE_KEY_PRE_COMPRA = "all_pre_products"
+
+
+class PreProduct {
+    constructor(name, finished) {
+        this.name = name
+        this.finished = finished
+    }
+
+    static getAllProducts = () => JSON.parse(localStorage.getItem(STORAGE_KEY_PRE_COMPRA))
+
+    static saveProduct(product) {
+        let allProducts = this.getAllProducts()
+
+        if (!allProducts)
+            allProducts = new Array()
+
+        allProducts.push(product)
+        localStorage.setItem(STORAGE_KEY_PRE_COMPRA, JSON.stringify(allProducts))
+    }
+
+    static insertProduct(product) {
+        let liProduct = document.createElement("li")
+        let inputCheck = document.createElement("input");
+        inputCheck.classList.add("form-check-input")
+        inputCheck.type = "checkbox"
+        inputCheck.id = "checkbox_" + product.name;
+        inputCheck.addEventListener("click", new PreProduct().#event_updateProductStatus);
+
+        let label = document.createElement("label");
+        label.classList.add("form-check-label");
+        label.for = inputCheck.id;
+        label.appendChild(document.createTextNode(product.name));
+        label.addEventListener("dblclick", new PreProduct().#event_removeProduct);
+
+        if (product.finished) {
+            label.classList.add("line-through");
+            inputCheck.checked = true;
+        }
+        liProduct.appendChild(inputCheck);
+        liProduct.appendChild(label);
+        document.getElementById("all_pre_products").appendChild(liProduct);
+    }
+
+    #event_removeProduct(evt) {
+        if (confirm("¿Seguro que deseas eliminar el producto de la lista?")) {
+            const label = evt.srcElement
+            const nameProduct = label.textContent
+            // remove product from Storage:
+            PreProduct.removeProduct(nameProduct)
+            // remove product from view:
+            label.parentElement.parentElement.removeChild(label.parentElement)
+        }
+    }
+
+    #event_updateProductStatus(evt) {
+        const element = evt.srcElement;
+        let productName = element.id.split('_')[1];
+        // remove line from label:
+        const label = element.parentElement.childNodes[1]
+        // debugger;
+        if (element.checked) {
+            label.classList.add("line-through");
+        } else {
+            label.classList.remove("line-through");
+        }
+        // updated preProduct in local storage:
+        PreProduct.updateProductStatus(productName);
+    }
+
+    static removeProduct(productName) {
+        const allProducts = this.getAllProducts()
+
+        for (let index in allProducts) {
+            if (allProducts[index].name == productName) {
+                allProducts.splice(index, 1)
+                break
+            }
+        }
+        localStorage.setItem(STORAGE_KEY_PRE_COMPRA, JSON.stringify(allProducts))
+    }
+
+    static updateProductStatus(productName) {
+        const allProducts = this.getAllProducts()
+        for (let product of allProducts) {
+            if (product.name === productName) {
+                product.finished = (!product.finished)
+                break
+            }
+        }
+
+        localStorage.setItem(STORAGE_KEY_PRE_COMPRA, JSON.stringify(allProducts))
+    }
+
+}
 
 
 const btnNewPreProduct = document.getElementById("add_new_pre_product")
 const inputNewPreProduct = document.getElementById("newPreProduct")
 
-const showAllPreProducts = () => {
-    const allPreProducts = PreProduct.getAllPreProducts()
-
-    if (allPreProducts !== null && allPreProducts.length > 0) {
-        allPreProducts.forEach(product => PreProduct.insertPreProduct(product))
-    }
-}
-
-addEventListener("load", () => {
-    // setActiveNavItem()
-    showAllPreProducts()
-})
-
-const event_newPreProduct = (evt) => {
-    if (inputNewPreProduct.value) {
-        const product = new PreProduct(inputNewPreProduct.value, false)
-        PreProduct.insertPreProduct(product)
-        PreProduct.savePreProduct(product)
-        inputNewPreProduct.value = ""
-    }
-}
 
 btnNewPreProduct.addEventListener("click", (evt) => {
     evt.preventDefault()
-    event_newPreProduct()
+    const name = inputNewPreProduct.value
+    const price = inputNewPreProduct.value
+
+    if (name && price) {
+        const product = new PreProduct(name, false)
+        PreProduct.insertProduct(product)
+        PreProduct.saveProduct(product)
+
+        inputNewPreProduct.value = ""
+        inputNewPreProduct.value = ""
+    }
 })
 
-inputNewPreProduct.addEventListener("keypress", (evt) => {
-    if (evt.key === "Enter") event_newPreProduct()
+
+addEventListener("DOMContentLoaded", () => {
+    const allProducts = PreProduct.getAllProducts()
+
+    if (allProducts !== null && allProducts.length > 0) {
+        allProducts.forEach(product => PreProduct.insertProduct(product))
+    }
 })
-
-
 
